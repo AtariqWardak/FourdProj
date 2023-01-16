888aefe3c6e15bb90be64c85046156794bacc5ce
Aman <Aman@LAPTOP-BDBCPBRD>
2023-01-15 22:09:43 -0500

    A

--- src/main/java/Salamoona.java
+++ src/main/java/Salamoona.java
@@ -1,5 +1,5 @@
 public class Salamoona {
     public static void main(String[] args) {
-        System.out.println("Hello khjkjiydfgddfgdgdfgdgfguiyihworld");
+        System.out.println("Hfgdgdfgdgfguiyihworld");
     }
 }
